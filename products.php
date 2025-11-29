4cc2a8e79cdd1a3be2dbd7e879a729f6b956d237
Sanchez-Alexa <shairacapiral@gmail.com>
2025-11-29 15:04:53 +0800

    Add initial products list and store information

---
+++ products.php
@@ -0,0 +1,12 @@
+<?php
+$storeName = "Techy Essentials";
+$shippingFee = 50;
+$isOpen = true;
+
+$products = [
+    ["name" => "Wireless Mouse", "price" => 350],
+    ["name" => "Mechanical Keyboard", "price" => 1499],
+    ["name" => "Wireless Headphone", "price" => 299],
+    ["name" => "USB Type-C Cable", "price" => 120],
+];
+?>
